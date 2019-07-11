565ad736fbdf21dda9380ae2fe9332cebc4b31d5
Anish Shenoy <shenoy.anish.227@gmail.com>
2019-07-11 17:36:27 -0400

    DONE

--- testapi.py
+++ testapi.py
@@ -56,8 +56,8 @@ def getUserPref(user_name):
     return user_data
 
 #Helper function for matching company preferences to user
-def getCompanyList(db_name, user_name):
-    user_data = getUserPref()
+def getCompanyList(user_name):
+    user_data = getUserPref(user_name)
     company_data = getData(COMPANY_TABLE, 0, "list")
 
     company_to_percent = {}
@@ -89,6 +89,33 @@ def check_account_exist(username):
     db_connection.close()
     return False
 
+def create_account(user_name, password):
+    print user_name
+    print password
+    db_connection = sqlite3.connect(USER_TABLE)
+    db_crsr = db_connection.cursor()
+    command = "INSERT INTO 'users' (username, password) VALUES "+"('" + user_name + "'" + ",'" + password + "');"
+    print command
+    db_crsr.execute(command)
+    db_connection.commit()
+    db_connection.close()
+    return True
+
+def addData(username, abortion,anti_poverty,made_in_us,lgbtq_support,sustainable,animal_cruel, veteran_support, gun_control):
+   db_name = "'users'"
+   db_connection = sqlite3.connect(USER_TABLE)
+   db_crsr = db_connection.cursor()
+   user = "'%s'" % username
+#UPDATE 'users' SET abortion = 3 WHERE username = 'anish';
+
+   db_crsr.execute("UPDATE" + db_name + "SET " + "abortion = " + str(abortion) + "," + "anti_poverty = " +
+   str(anti_poverty) + "," + "made_in_us = " + str(made_in_us) + "," + "lgbtq_support = " + str(lgbtq_support) + "," +
+   "sustainable = " + str(sustainable) + "," + "animal_cruel = " + str(animal_cruel) + "," + "veteran_support = " + str(veteran_support) + "," + "gun_control = " + str(gun_control) +
+   " WHERE" + " username = " + user)
+   db_connection.commit()
+   db_connection.close()
+   return True
+
 #ENDPOINTS
 
 '''
@@ -111,75 +138,62 @@ def check_account_exist(username):
 '''
 @app.route('/companies/<company>', methods=['GET'])
 def companySearch(company):
-      return json.dumps(getData('company_data.db', company_name,"single"))
+      return json.dumps(getData('company_data.db', company,"single"))
 
 
 '''
-#Returns a list of top 10 companies and perecntages of how much they match to you
-#Endpoint: /users/companyList
-#Takes in 'username'
-#Example Return:
-[
-  [
-    "Johnson & Johnson",
-    37.5
-  ],
-  [
-    "Amazon",
-    37.5
-  ],
-  [
-    "Morgan Stanley",
-    37.5
-  ],
-  [
-    "Boeing",
-    37.5
-  ],
-  [
-    "Accenture",
-    37.5
-  ],
-  [
-    "IBM",
-    25.0
-  ],
-  [
-    "Cisco Systems, Inc.",
-    25.0
-  ],
-  [
-    "Kaiser Permanente",
-    25.0
-  ],
-  [
-    "General Motors",
-    25.0
-  ]
-]
-'''
-@app.route('/users/companyList', methods=['GET', 'POST'])
-def companyList(username):
-   if request.method == 'POST':
+Endpoint: /users/<username>
+For GET requests:
+    #Example Return:
+    [
+      [
+        "Johnson & Johnson",
+        37.5
+      ],
+      [
+        "Amazon",
+        37.5
+      ],
+      [
+        "Morgan Stanley",
+        37.5
+      ],
+      [
+        "Boeing",
+        37.5
+      ],
+      [
+        "Accenture",
+        37.5
+      ],
+      [
+        "IBM",
+        25.0
+      ],
+      [
+        "Cisco Systems, Inc.",
+        25.0
+      ],
+      [
+        "Kaiser Permanente",
+        25.0
+      ],
+      [
+        "General Motors",
+        25.0
+      ]
+    ]
 
-      user_name = request.form['username']
-
-      return getCompanyList('user_table.db', user_name)
-
-   else:
-
-      return "err"
-
-'''
-Endpoint: /users/auth
-Takes in 'username' and 'password'
-Returns either True or False
+For POST requests:
+    Takes in a 'password' and authenticates username and password
+    Returns True or False
 '''
 @app.route('/users/<username>', methods=['GET', 'POST'])
 def users(username):
     if request.method == "GET":
-        return getCompanyList('user_table.db', username)
+        return getCompanyList(username)
     if request.method == "POST":
+        password = request.form["password"]
         if (not check_account_exist(username)):
             return jsonify(False)
         db_connection = sqlite3.connect(USER_TABLE)
@@ -189,15 +203,56 @@ def users(username):
         db_connection.close()
         return jsonify(password == passdb[0])
 
+'''
+Endpoint: /users/<username>/preferences/
+For GET requests:
+    #Example
+    {
+      "abortion": 1,
+      "animal_cruel": 1,
+      "anti_poverty": 1,
+      "gun_control": 1,
+      "lgbtq_support": -1,
+      "made_in_us": 1,
+      "sustainable": 0,
+      "veteran_support": -1
+    }
+For POST requests:
+    Takes in "abortion", "anti_poverty", "made_in_us", "lgbtq_support", "sustainable", "animal_cruel", "veteran_support", "gun_control"
+    returns True or False
+'''
+@app.route('/users/<username>/preferences', methods=['GET', 'POST'])
+def prefs(username):
+    if request.method == "GET":
+        user_list = getUserPref(username)
+        topics = ["abortion", "anti_poverty", "made_in_us",	"lgbtq_support", "sustainable",	"animal_cruel", "veteran_support", "gun_control"]
+        user_dict = {}
+        for i in range(len(user_list)):
+            user_dict[topics[i]] = user_list[i]
+        return jsonify(user_dict)
+    if request.method == "POST":
+        abortion = request.form['abortion']
+        anti_poverty = request.form['anti_poverty']
+        made_in_us = request.form['made_in_us']
+        lgbtq_support = request.form['lgbtq_support']
+        sustainable = request.form['sustainable']
+        animal_cruel = request.form['animal_cruel']
+        veteran_support = request.form['veteran_support']
+        gun_control = request.form['gun_control']
+        return jsonify(addData(username, abortion,anti_poverty,made_in_us,lgbtq_support,sustainable,animal_cruel, veteran_support, gun_control))
 
-
-@app.route('/users/createAccount', methods=['GET', 'POST'])
-def createAccount(user_name,password):
-
-    #switch to json http get/post
-    #success fail message =
-
-    return "success fail message"
+'''
+Endpoint: /users/createAccount
+For POST requests:
+    Takes in 'username' and 'password'
+    Returns true or false
+'''
+@app.route('/users/createAccount', methods=['POST'])
+def createAccount():
+    if request.method == 'POST':
+      user_name = request.form['username']
+      password = request.form['password']
+      return jsonify(create_account(user_name,password))
 
 if __name__ == "__main__":
    app.debug = True
